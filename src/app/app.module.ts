104ddf557be33b9304d1514759bb10f088482400
vates_abiss <vates_abiss@lnexternos.com.ar>
2020-07-04 00:53:24 -0300

    Primer commit ap sin effects

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
+// Módulos personalizados
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    UsuariosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
