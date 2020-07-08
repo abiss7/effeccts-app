efb206871442cd1c634ce984957a6b0c49e9b1df
vates_abiss <vates_abiss@lnexternos.com.ar>
2020-07-08 12:28:39 -0300

    effects terminado

---
+++ src/app/store/effects/usuario.effects.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import * as usuariosActions from '../actions';
+import { tap, mergeMap, map, catchError } from 'rxjs/operators';
+import { UsuarioService } from '../../services/usuario.service';
+import { of } from 'rxjs';
+
+
+@Injectable()
+export class UsuarioEffects {
+
+    constructor(
+        private actions$: Actions,
+        private usuariosService: UsuarioService
+    ) {}
+
+    cargarUsuario$ = createEffect(
+
+        () => this.actions$.pipe(
+            ofType( usuariosActions.cargarUsuario ),
+            mergeMap(
+                ( action ) => this.usuariosService.getUserById( action.id )
+                    .pipe(
+                        map(user => usuariosActions.cargarUsuarioSuccess({ usuario: user })),
+                        catchError(err => of(usuariosActions.cargarUsuarioError({ payload: err })))
+                    )
+            )
+        )
+    );
+}
--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducers';
+import { cargarUsuario } from '../../store/actions/usuario.actions';
+import { Usuario } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-usuario',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario;
+  loading: boolean = false;
+  loaded: boolean = false;
+  error: any;
+
+  constructor(
+    private router: ActivatedRoute,
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
+
+    this.store.select('usuario')
+      .subscribe(({user, loading, loaded, error}) => {
+
+        this.usuario = user;
+        this.loading = loading;
+        this.loaded = loaded;
+        this.error = error;
+      });
+
+    this.router.params.subscribe(({ id }) => {
+
+      this.store.dispatch(cargarUsuario( { id } ));
+    });
   }
 
 }
